9717f36c5bc5d78dd109bf24124125768009c0e5
babayybay <147687642+babay162@users.noreply.github.com>
2025-04-03 16:52:22 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=FakeDreamer Kernel by Re-Noroi
+kernel.string=~RealOverissue Kernel by Babay
 do.devicecheck=1
 do.modules=0
 do.systemless=1
